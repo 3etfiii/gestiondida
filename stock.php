c7bd2def086512cce7cb1424584f6e8545b9ef54
3etfiii <jad.bousfiha@gmail.com>
2020-07-29 11:34:06 +0100

    Add files via upload
    je veux faire une gestion de stock et je suis vraiment bloqué

---
+++ stock.php
@@ -0,0 +1,54 @@
+<html>
+  <head>
+        <title>DiDa Group </title>
+      <link rel="icon" href="https://www.didaetoileservice.ma/wp-content/uploads/2020/01/LogoDidaSite.png" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css" />
+  
+  </head>
+  <body>
+      <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <a class="navbar-brand" href="index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="Achats.php">Achats <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Management.php">Management</a>
+      </li>
+       <li class="nav-item">
+           <a class="nav-link" href="Logistique.php">Logistique</a>
+      </li>
+       <li class="nav-item">
+           <a class="nav-link" href="hygiene.php">hygiène</a>
+      </li>
+       <li class="nav-item">
+           <a class="nav-link" href="stock.php">Stock</a>
+      </li>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+      
+      <h1 >   
+      
+        </h1>
+     
+ 
+      <button class="btn btn-primary" type="button" href="ajoutP.php">Ajouter un produit</button>
+      <button class="btn btn-primary" type="submit">Modifier un produit</button>
+      <button class="btn btn-primary" type="submit">Supprimer un produit</button>
+      <button class="btn btn-primary" type="submit">Afficher tous les produits du stock</button>
+      <button class="btn btn-primary" type="submit">Rechercher un produit</button>
+      
+
